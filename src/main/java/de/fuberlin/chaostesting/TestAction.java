c709c75a4cd7afe551b86156bd01f27dbba9a113
Danni Otterbach <danni.otterbach@googlemail.com>
2016-08-19 01:10:19 +0200

    remove duplicate object properties

--- src/main/java/de/fuberlin/chaostesting/TestAction.java
+++ src/main/java/de/fuberlin/chaostesting/TestAction.java
@@ -5,10 +5,12 @@ import java.util.Date;
 import de.fuberlin.chaostesting.hibernate.Test;
 import net.sourceforge.stripes.action.ActionBean;
 import net.sourceforge.stripes.action.ActionBeanContext;
+import net.sourceforge.stripes.action.After;
 import net.sourceforge.stripes.action.DefaultHandler;
 import net.sourceforge.stripes.action.ForwardResolution;
 import net.sourceforge.stripes.action.Resolution;
 import net.sourceforge.stripes.action.UrlBinding;
+import net.sourceforge.stripes.controller.LifecycleStage;
 import net.sourceforge.stripes.validation.Validate;
 import net.sourceforge.stripes.validation.ValidationErrors;
 
@@ -17,13 +19,8 @@ public class TestAction implements ActionBean {
 
 	ActionBeanContext context;
 	
-	String von;
-	String nach;
-	Date hinfahrt;
-	String reisende;
-	String klasse;
-	String preis;
 	String result;
+	Test test;
 	
 	@Override
 	public void setContext(ActionBeanContext context) {
@@ -35,54 +32,14 @@ public class TestAction implements ActionBean {
 		return context;
 	}
 	
-	public String getVon() {
-		return von;
+	public Test getTest() {
+		return test;
 	}
 
-	public void setVon(String von) {
-		this.von = von;
+	public void setTest(Test test) {
+		this.test = test;
 	}
 
-	public String getNach() {
-		return nach;
-	}
-
-	public void setNach(String nach) {
-		this.nach = nach;
-	}
-
-	public Date getHinfahrt() {
-		return hinfahrt;
-	}
-
-	public void setHinfahrt(Date hinfahrt) {
-		this.hinfahrt = hinfahrt;
-	}
-
-	public String getReisende() {
-		return reisende;
-	}
-
-	public void setReisende(String reisende) {
-		this.reisende = reisende;
-	}
-
-	public String getKlasse() {
-		return klasse;
-	}
-
-	public void setKlasse(String klasse) {
-		this.klasse = klasse;
-	}
-
-	public String getPreis() {
-		return preis;
-	}
-
-	public void setPreis(String preis) {
-		this.preis = preis;
-	}
-	
 	public String getResult() {
 		return result;
 	}
@@ -93,13 +50,8 @@ public class TestAction implements ActionBean {
 
 	@DefaultHandler
 	public Resolution createTest() {
-		new Test(von, nach, hinfahrt, reisende, klasse, preis).register();
+		test.register();
 		setResult("Test-Definition erfolgreich");
 		return new ForwardResolution("/testDefinition.jsp");
-	}
-	
-	@Validate(on="senden")
-	public void validateInput(ValidationErrors errors){
-		
 	}
 }
--- src/main/webapp/testDefinition.jsp
+++ src/main/webapp/testDefinition.jsp
@@ -10,27 +10,27 @@
 	</h3>
 	<stripes:form beanclass="de.fuberlin.chaostesting.TestAction">
 		<stripes:errors/>
-		Von: <stripes:text name="von"></stripes:text><br /><br />
-		Nach: <stripes:text name="nach"/><br /><br />
-		Datum: <stripes:text name="hinfahrt" formatType="date" formatPattern="YYYY-MM-dd" value="jjjj-mm-tt"/><br /><br />
+		Von: <stripes:text name="test.testVon"></stripes:text><br /><br />
+		Nach: <stripes:text name="test.testNach"/><br /><br />
+		Datum: <stripes:text name="test.testHinfahrt" formatType="date" formatPattern="YYYY-MM-dd" value="jjjj-mm-tt"/><br /><br />
 		Reisende:
-		<stripes:select name="reisende" size="1">
+		<stripes:select name="test.testReisende" size="1">
 			<stripes:option value="1">1 Reisender</stripes:option>
 			<stripes:option value="2">2 Reisende</stripes:option>
 			<stripes:option value="3">3 Reisende</stripes:option>
 			<stripes:option value="4">4 Reisende</stripes:option>
 			<stripes:option value="5">5 Reisende</stripes:option>
 		</stripes:select><br/><br/>
-		Klasse 1: <stripes:radio name="klasse" value="1"/><br />
-		Klasse 2: <stripes:radio name="klasse" value="2"/><br /><br />
-		Preis (Sollwert): <stripes:text name="preis" value="0"/><br /><br />
+		Klasse 1: <stripes:radio name="test.testKlasse" value="1" checked="checked"/><br />
+		Klasse 2: <stripes:radio name="test.testKlasse" value="2"/><br /><br />
+		Preis (Sollwert): <stripes:text name="test.testPreis" value="0"/><br /><br />
 		<stripes:submit name="Eintragen" value="senden"/><br /> <br/>
 		<c:if test="${actionBean.result != null}">
 			<b>${actionBean.result}</b><br/>
-			Von ${actionBean.von} nach ${actionBean.nach}<br/>
-    		Datum: ${actionBean.hinfahrt}<br/>
-		   	Reisende: ${actionBean.reisende} Klasse: ${actionBean.klasse}<br/><br/>
-		   	Preis (Sollwert): ${actionBean.preis}<br />
+			Von ${actionBean.test.testVon} nach ${actionBean.test.testNach}<br/>
+    		Datum: ${actionBean.test.testHinfahrt}<br/>
+		   	Reisende: ${actionBean.test.testReisende} Klasse: ${actionBean.test.testKlasse}<br/><br/>
+		   	Preis (Sollwert): ${actionBean.test.preis}<br />
 		</c:if>
 		
 	</stripes:form>
