06e17515dc6bd301bd4521826d0052d089af8bb0
xyz <merz.xyz@gmail.com>
2016-08-25 13:50:42 +0200

    json, input, xml

---
+++ src/main/java/de/fuberlin/chaostesting/rest/TestObject.java
@@ -0,0 +1,18 @@
+package de.fuberlin.chaostesting.rest;
+
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlElement;
+
+
+@XmlRootElement(name="test")
+//@XmlAccessorType(XmlAccessType.FIELD)
+public class TestObject {
+
+	@XmlElement(name="id")    
+	private String testId;
+
+	public TestObject(String testId) {
+		this.testId = testId;
+	}
+}
