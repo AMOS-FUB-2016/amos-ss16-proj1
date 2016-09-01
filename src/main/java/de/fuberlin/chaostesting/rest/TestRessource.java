c81eaafda4d7cc188aa55dc7d6df7794de20a1f6
xyz <merz.xyz@gmail.com>
2016-09-01 12:33:12 +0200

    Reparatur Rest

---
+++ src/main/java/de/fuberlin/chaostesting/rest/TestRessource.java
@@ -0,0 +1,72 @@
+package de.fuberlin.chaostesting.rest;
+
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/test")
+public class TestRessource {
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String testText() {
+		return "TestText";
+	}
+
+	@GET
+	@Produces(MediaType.TEXT_XML)
+	public String testXML() {
+		return "<?xml version=\"1.0\"?>" + "<test> Test" + "</test>";
+	}
+
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String testHTML() {
+		return "<html> " + "<title>" + "TEST" + "</title>"
+				+ "<body><h1>" + "Test" + "</body></h1>" + "</html> ";
+	}
+	
+	@GET
+	@Path("{codeVersion}-{dataVersion}-{testId}")
+	@Produces(MediaType.TEXT_HTML)
+	public String testValue(@PathParam("codeVersion") String codeVersion, @PathParam("dataVersion") String dataVersion, @PathParam("testId") String testId) {
+		return "<html> " + "<title>" + "TEST" + "</title>"
+				+ "<body><h1>" + "Test" + "</body></h1>" + 
+				
+				"<br>Code-Version = "+codeVersion+"<br><br>Data-Version = "+dataVersion+"<br><br>Test-Id = "+testId +
+			
+				"</html> ";
+		//TextPlain
+		//return "Code-Version="+codeVersion+" Data-Version="+dataVersion+" Test-Id="+testId;
+	}
+
+	@GET
+	@Path("{testID}")
+	@Produces(MediaType.TEXT_XML)
+	public TestObject getTest (@PathParam("testId") String testId) {
+		//return new TestObject(testId);
+		TestObject test = new TestObject();
+		test.setValue("0");
+	    return test;		
+	}
+
+	@GET
+	@Path("/jsn")
+	@Produces(MediaType.APPLICATION_JSON)
+	public TestObject testAutoJSON() {
+		//return new TestObject(testId);
+		TestObject test = new TestObject();
+		test.setValue("1");
+	    return test;
+	}
+	
+	@GET
+	@Path("/json")
+	@Produces(MediaType.APPLICATION_JSON)
+	public String testJSON() {
+		return "Test";
+	}
+}
