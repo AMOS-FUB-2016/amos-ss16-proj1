13eae3d117a5d4c8a8198bd7b03916641d32841d
Danni Otterbach <danni.otterbach@googlemail.com>
2016-09-04 13:35:59 +0200

    add rest method implementations for test execution

--- src/main/java/de/fuberlin/chaostesting/rest/TestResultResponse.java
+++ src/main/java/de/fuberlin/chaostesting/rest/TestResultResponse.java
@@ -2,9 +2,58 @@ package de.fuberlin.chaostesting.rest;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
+import de.fuberlin.chaostesting.model.Response;
+
 @XmlRootElement
 public class TestResultResponse {
 	
+	String error;
+	Integer testId;
+	boolean valid;
+	String responseXML;
+
+	public String getError() {
+		return error;
+	}
+
+	public void setError(String error) {
+		this.error = error;
+	}
 	
+	public boolean isValid() {
+		return valid;
+	}
+
+	public void setValid(boolean valid) {
+		this.valid = valid;
+	}
+
+	public Integer getTestId() {
+		return testId;
+	}
+
+	public void setTestId(Integer testId) {
+		this.testId = testId;
+	}
+
+	public String getResponseXML() {
+		return responseXML;
+	}
+
+	public void setResponseXML(String responseXML) {
+		this.responseXML = responseXML;
+	}
 	
+	public void configureWithResult(Response response) {
+		configureWithResult(response, false);
+	}
+	
+	public void configureWithResult(Response response, boolean extended) {
+		this.valid = response.isValid();
+		this.testId = response.getTest().getId();
+		
+		if(extended) {
+			responseXML = response.getXml();
+		}
+	}
 }
