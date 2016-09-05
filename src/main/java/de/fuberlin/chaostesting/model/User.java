7ef4f33ac82035e07be36e709cd55d5482673712
Danni Otterbach <danni.otterbach@googlemail.com>
2016-09-05 19:56:40 +0200

    make jpa/hibernate create own sequences for manually generated types

--- src/main/java/de/fuberlin/chaostesting/model/User.java
+++ src/main/java/de/fuberlin/chaostesting/model/User.java
@@ -3,6 +3,7 @@ package de.fuberlin.chaostesting.model;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -10,7 +11,7 @@ import javax.persistence.Table;
 @Table(name="USER_INFORMATION")
 public class User {
 	
-	@Id	@GeneratedValue
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="user_id")
 	private int id;
 	@Column(name="user_name")
