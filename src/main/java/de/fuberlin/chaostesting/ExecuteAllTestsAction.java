9614f24bffa0342c095a56d47d8add5345f57055
padrowan <paderowan@gmail.com>
2016-09-05 22:17:15 +0200

    Update ExecuteAllTestsAction.java
    Nur wenn alle Abfragen valide sind dann ist auch der Test valide

--- src/main/java/de/fuberlin/chaostesting/ExecuteAllTestsAction.java
+++ src/main/java/de/fuberlin/chaostesting/ExecuteAllTestsAction.java
@@ -47,8 +47,7 @@ public class ExecuteAllTestsAction extends GenericActionBean {
 		int valid = 0;
 		for (Response response : responses) {
 			responseDao.create(response);
-			
-			if (response.isValid()) {
+			if (response.isValid().get(0) && response.isValid().get(1) ){
 				valid++;
 			}
 		}		
