3cf543cdcae56f2070c5fac57ff7eef05a08d122
Danni Otterbach <danni.otterbach@googlemail.com>
2016-09-06 23:57:32 +0200

    restore sequence for response model class

--- src/main/java/de/fuberlin/chaostesting/model/Response.java
+++ src/main/java/de/fuberlin/chaostesting/model/Response.java
@@ -7,6 +7,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -22,7 +23,7 @@ import com.deutschebahn.osst.v1_0.AngebotsAntwort;
 @Table(name="TEST_RESPONSE")
 public class Response {
 	
-	@Id	@GeneratedValue
+	@Id	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="response_id")
 	private int id;
 	
