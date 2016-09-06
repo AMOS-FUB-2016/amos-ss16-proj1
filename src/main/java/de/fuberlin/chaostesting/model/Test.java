0c83f55360ad73395faf8b6b5840708608756ea8
Dennis Griesert <dennisgriesert@gmail.com>
2016-09-06 16:01:57 +0200

    Fix ID generation strategy

--- src/main/java/de/fuberlin/chaostesting/model/Test.java
+++ src/main/java/de/fuberlin/chaostesting/model/Test.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -12,7 +13,7 @@ import javax.persistence.Table;
 @Table(name="TEST_INFORMATION")
 public class Test {
 	
-	@Id	@GeneratedValue
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
 	@Column(name="test_id")
 	private int id;
 	
