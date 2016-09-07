4fe8c19372470fda0952abb440759e8b20d4ceb4
Dennis Griesert <dennisgriesert@gmail.com>
2016-09-07 23:40:52 +0200

    Add another Bezugsangebot

--- src/main/java/de/fuberlin/chaostesting/model/BezAngebot.java
+++ src/main/java/de/fuberlin/chaostesting/model/BezAngebot.java
@@ -3,7 +3,8 @@ package de.fuberlin.chaostesting.model;
 public enum BezAngebot {
 	
 	QuerDurchsLand("Quer-durchs-Land-Ticket", "QUER-DURCHS-LAND-T"),
-	SchoenesWochenende("Schönes-Wochenende-Ticket", "Schönes-Wochenende-T");
+	SchoenesWochenende("Schönes-Wochenende-Ticket", "Schönes-Wochenende-T"),
+	IREBerlinHamburg("IRE-Berlin-Hamburg-Ticket", "IRE-Berlin-HH-Ticket");
 	
 	private final String publicName;
 	private final String internalName;
